5738e1ccb4778379622a3abcf789f24df2260834
trabajo\damoreno <damoreno@trabajo.gob.ar>
2022-04-26 22:48:03 -0300

    se saca pantallazo negro

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,165 @@
+import { Injectable, NgZone } from '@angular/core';
+import { User } from '../services/user';
+import * as auth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  userData: any; // Save logged in user data
+  constructor(
+    public afs: AngularFirestore, // Inject Firestore service
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+    public router: Router,
+    public ngZone: NgZone, // NgZone service to remove outside scope warning,
+    public toastCtrl: ToastController
+  ) {
+    /* Saving user data in localstorage when 
+    logged in and setting up null when logged out */
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
+  //toaster
+  presentToast(mensaje,duracion,position,color,clases) {
+    let toast = this.toastCtrl.create({
+      message: mensaje,
+      duration: duracion,
+      position: position,
+      cssClass: clases,
+      color:color,
+    }).then((toastData) => {
+      console.log(toastData);
+      toastData.present();
+    })
+    // .then((toastData)=>{
+    //   console.log(toastData)
+    //   this.toastCtrl.dismiss();
+    // })
+  
+    // toast.onDidDismiss();
+  
+    // toast.present();
+  }
+  // Sign in with email/password
+  async SignIn(email: string, password: string) {
+    try {
+      const result = await this.afAuth
+        .signInWithEmailAndPassword(email, password);
+      this.ngZone.run(() => {
+        this.router.navigate(['dashboard']);
+        this.presentToast("Logueo exitoso.",2000,'bottom','success','text-center');
+      });
+      this.SetUserData(result.user);
+    } catch (error:any) {
+      console.log(error.message);
+      this.presentToast('Contraseña o email invalidos, chequee los datos ingresados.',2000,'bottom','danger','text-center');
+    }
+  }
+  // Sign up with email/password
+  async SignUp(email: string, password: string) {
+    try {
+      const result = await this.afAuth
+        .createUserWithEmailAndPassword(email, password);
+      /* Call the SendVerificaitonMail() function when new user sign
+      up and returns promise */
+      this.SendVerificationMail();
+      this.SetUserData(result.user);
+    } catch (error:any) {
+      window.alert(error.message);
+    }
+  }
+  // Send email verfificaiton when new user sign up
+  async SendVerificationMail() {
+    return this.afAuth.currentUser
+      .then((u: any) => u.sendEmailVerification())
+      .then(() => {
+        this.router.navigate(['verify-email-address']);
+      });
+  }
+  // Reset Forggot password
+  async ForgotPassword(passwordResetEmail: string) {
+    return this.afAuth
+      .sendPasswordResetEmail(passwordResetEmail)
+      .then(() => {
+        window.alert('Password reset email sent, check your inbox.');
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+  // Returns true when user is looged in and email is verified
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
+  }
+  // Sign in with Google
+  async GoogleAuth() {
+    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
+      if (res) {
+        this.router.navigate(['dashboard']);
+      }
+    });
+  }
+  // Auth logic to run auth providers
+  async AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+  /* Setting up user data when sign in with username/password, 
+  sign up with username/password and sign in with social auth  
+  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+  // Sign out
+  async SignOut() {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['sign-in']);
+    });
+  }
+}
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+
+//   constructor() { }
+// }
